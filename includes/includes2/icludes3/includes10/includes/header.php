e2de3986e76bd3ba1aba1e37d44f91260916a284
zelenkroki <zelenkroki@dir.bg>
2013-10-22 06:08:17 +0300

    Create header.php

---
+++ includes/includes2/icludes3/includes10/includes/header.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Biblioteka bez tekstove</title>
+        <meta charset="UTF-8">       
+    </head>
+    <body>
+        <p>
+            <a href="index.php">Записани книги в каталога</a> |
+            <a href="dobavkaBook.php">Добавяне на книга</a> | 
+            <a href="dobavkaAvtor.php">Добавяне на автор</a>
+        </p>  
