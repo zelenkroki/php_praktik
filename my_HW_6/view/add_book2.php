26a7f79ed9cf2b2ca4b6f643aeb79bc5bcea4d62
zelenkroki <zelenkroki@dir.bg>
2013-11-03 22:51:49 +0200

    Create add_book2.php

---
+++ my_HW_6/view/add_book2.php
@@ -0,0 +1,28 @@
+<?php
+$tiltle = 'Нова книга';
+include './includes/header.php';
+
+include '../business_logic/add_book_secret.php';
+echo nova_kniga($db);
+?>
+<a href="index.php">Списък</a>
+
+<form method="post" action="add_book.php">
+    Име: <input type="text" name="book_name" />
+
+    <div>Автори:<select name="authors[]" multiple style="width: 200px">
+            <?php
+            foreach ($authors as $row) {
+                echo '<option value="' . $row['author_id'] . '">
+                    ' . $row['author_name'] . '</option>';
+            }
+            ?>
+
+        </select></div>
+    <input type="submit" value="Добави" />    
+
+</form>
+
+<?php
+include './includes/footer.php';
+?>
